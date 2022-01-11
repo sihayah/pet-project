5f480a0fb6f0d812e482d7642dc1b18fe78ac434
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-01-11 18:40:40 -0500

    added nodemailer and functional email send

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment, Post } = require('../../models');
 const withAuth = require('../../utils/auth');
-const Email = require('email-templates');
+//const Email = require('email-templates');
+const nodemailer = require("nodemailer");
 
-const email = new Email ({
+
+/*const email = new Email ({
   message: {
     from: 'kohn.max@gmail.com'
   },
@@ -11,7 +13,7 @@ const email = new Email ({
   transport: {
     jsonTransport: true
   }
-});
+});*/
 
 router.get('/', (req, res) => {
   Comment.findAll()
@@ -30,7 +32,61 @@ router.post('/', withAuth, (req, res) => {
     post_id: req.body.post_id
   })
     .then(dbCommentData => {
-      email.send({
+      console.log(dbCommentData.dataValues.post_id);
+      /*//figure out how to get post user email address and username
+      router.get('/:id', (req, res) => {
+        Post.findOne({
+          where: {
+            id: dbCommentData.dataValues.post_id
+          },
+          attributes: ['id'],
+          include: [
+            {
+              model: User,
+              attributes: ['username', 'email']
+            }
+          ]
+        })
+          .then(dbPostData => {
+            if (!dbPostData) {
+              res.status(404).json({ message: 'No post found with this id' });
+              return;
+            }
+            console.log(dbPostData);
+            res.json(dbPostData);
+          })
+          .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+          });
+      });*/
+      async function sendEmail() {
+        // Generate test SMTP service account from ethereal.email
+      
+        const transporter = nodemailer.createTransport({
+          host: 'smtp.ethereal.email',
+          port: 587,
+          auth: {
+              user: 'maybelle.howell90@ethereal.email',
+              pass: 'wqK5HrumAzPfGSnnG7'
+          }
+      });
+      
+        // send mail with defined transport object
+        let info = await transporter.sendMail({
+          from: '"Comment Ghost 👻"', // sender address
+          to: req.session.email, // currently commenters email address
+          subject: `Max has just received a comment on a post`, // Subject line
+          html: `<p>Hi ${req.session.username},<p><br><p>You've just receieved a comment on <a href="https://agile-hamlet-39263.herokuapp.com/post/${req.body.post_id}">this</a> post</p>` // html text body
+        });
+
+      }
+
+      sendEmail().catch(console.error);
+
+
+
+      /*email.send({
         template: 'comment-notification',
         message: {
           to: req.session.email
@@ -39,9 +95,11 @@ router.post('/', withAuth, (req, res) => {
           name: req.session.username,
           posturl: `https://agile-hamlet-39263.herokuapp.com/post/${req.body.post_id}`
         }
-      }).catch(e => {
+      })
+      .then(console.log('email sent'))
+      .catch(e => {
         console.log(e);
-      });
+      });*/
       res.json(dbCommentData);
     })
     .catch(err => {

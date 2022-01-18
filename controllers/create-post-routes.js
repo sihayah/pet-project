ee2173323fa7399807f300023a6a0e7da6d824f5
pcancio <pcancio@gmail.com>
2022-01-17 22:05:54 -0500

    22:05pm edits to routes

---
+++ controllers/create-post-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../../models');
+const withAuth = require('../utils/auth');
+
+router.get('/create-post', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('create-post');
+});
+module.exports = router;

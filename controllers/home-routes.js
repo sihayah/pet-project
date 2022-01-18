abf423ac9e8229fb89dfb6c48c7ad0742b38baf2
Sihayah <sihaya_a@hotmail.com>
2022-01-18 06:16:12 -0500

    home-routes order by top results

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,43 +2,54 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment, Vote } = require('../models');
 
-// get all posts for homepage
+// get top ten posts and organize by vote_count
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-            attributes: [
-                'id',
-                'description',
-                'pet_name',
-                'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-            ],
-            include: [{
-                    model: Comment,
-                    attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-                    include: {
-                        model: User,
-                        attributes: ['username']
-                    }
-                },
-                {
-                    model: User,
-                    attributes: ['username']
-                }
-            ]
-        })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-
-            res.render('homepage', {
-                posts,
-                loggedIn: req.session.loggedIn
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+      limit: 10,
+      attributes: [
+        'id',
+        'description',
+        'pet_name',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      order: [[
+          sequelize.col('vote_count'), 
+          'DESC']],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+  
+        // filter posts according to key value
+        const filteredPosts = posts.filter(post => post.id === 3);
+        console.log(filteredPosts)
+  
+        res.render('homepage', {
+          posts,
+          filteredPosts,
+          loggedIn: req.session.loggedIn
         });
-});
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 // get single post
 router.get('/post/:id', (req, res) => {
